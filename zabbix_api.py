db2b7e9a94e78eab0dabe0a08c0c552379cd4ba8
Janssen dos Reis Lima <janssenreislima@gmail.com>
2013-11-19 18:18:12 -0200

    Original https://github.com/gescheit/scripts/tree/master/zabbix/

--- zabbix_api.py
+++ zabbix_api.py
@@ -20,7 +20,6 @@
 #License along with this library; if not, write to the Free Software
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-
 # NOTES:
 # The API requires zabbix 1.8 or later.
 # Currently, not all of the API is implemented, and some functionality is
