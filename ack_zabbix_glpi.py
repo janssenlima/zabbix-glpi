625c6434524179606f021c621517124270eae799
Janssen dos Reis Lima <janssenreislima@gmail.com>
2015-05-11 08:10:17 -0300

    Update ack_zabbix_glpi.py

--- ack_zabbix_glpi.py
+++ ack_zabbix_glpi.py
@@ -1,16 +1,16 @@
 ## Autor: Janssen dos Reis Lima <janssenreislima@gmail.com>
-## Ultima atualizacao: 20/11/2013
+## Ultima atualizacao: 11/05/2015
 ## Execucao: python ack_zabbix_glpi.py <eventid> <ticket_glpi>
 ## Observacoes: Este script eh executado automaticamente apos a abertura do ticket no GLPI
 
 from zabbix_api import ZabbixAPI
 import sys
  
-server = "http://localhost/zabbix" # Endereco ou Ip do servidor do zabbix
-username = "admin"              # Usuario
-password = "zabbix"     # Senha
+server = "http://localhost/zabbix"
+username = "admin"             
+password = "zabbix"     
  
-conexao = ZabbixAPI(server = server, path="", log_level=6)
+conexao = ZabbixAPI(server = server)
 conexao.login(username, password)
 
 reconhecer_evento = conexao.event.acknowledge({"eventids": sys.argv[1], "message": "Ticket " + str(sys.argv[2]) + " criado no GLPI."})
